0e1d5774247963231ba6a02587ff7c1b0805c961
Tommy Jeff <threcius@yahoo.com>
2026-02-25 09:51:45 +0800

    Fixed lib error

--- src/cocoadock.cr
+++ src/cocoadock.cr
@@ -1,4 +1,4 @@
-@[Link(ldflags: "-framework Cocoa -framework WebKit -framework Foundation #{__DIR__}/../ext/cocoawebview.m")]
+@[Link(ldflags: "-framework Cocoa -framework WebKit -framework Foundation #{__DIR__}/../ext/cocoadock.m")]
 lib Native
   fun add(a : Int32, b : Int32) : Int32
 end
